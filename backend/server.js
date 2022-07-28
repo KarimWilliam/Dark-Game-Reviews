5891028e7e955f9f0f21b53a65c5708e4dd4a7f4
KarimWilliam <karim.william@gmail.com>
2022-07-28 12:26:38 +0200

    hmm

--- backend/server.js
+++ backend/server.js
@@ -20,17 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/reviews', require('./routes/reviewRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-// //Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.get('*', (req, res) =>
-//       res.sendFile(
-//         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-//       )
-//     );
-//   } else {
-//     app.get('/', (req, res) => res.send('Please set to production'));
-//   }
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 
 app.use(errorHandler);
