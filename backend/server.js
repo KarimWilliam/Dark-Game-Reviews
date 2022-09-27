5bb7e51b1ea2c0763e1f33a879eeade8a4e61a5b
dark <karim.william7@gmail.com>
2022-09-27 18:12:52 +0200

    qol changes

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  app.get("/", (req, res) => res.send("Please set to production"));
+  app.get("/", (req, res) => res.send("Please set to production build"));
 }
 
 app.use(errorHandler);
